622613894ff146199102cf4b7223fac453fbe3e6
Kaelan Bartlett <228972983+Helodity-M@users.noreply.github.com>
2025-11-18 19:57:37 -0500

    Add easing to SongPulser

--- Assets/Script/SongPulser.cs
+++ Assets/Script/SongPulser.cs
@@ -20,6 +20,7 @@ public class SongPulser : MonoBehaviour
         float beats = player.GetCurrentBeatNumber();
         float fraction = beats % 1;
         if(fraction < 0) fraction += 1;
+        fraction = Mathf.Sin((fraction * Mathf.PI) / 2); // Add Easing
         transform.localScale = Vector3.Lerp(baseScale * (1 + Amplitude), baseScale, fraction);
     }
 }
